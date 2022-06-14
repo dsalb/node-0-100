d09a3bc044541e33b701a6116c9442c4af12fda6
Diego <dsalbarrey@gmail.com>
2022-06-13 22:26:25 -0300

    Se quitan archivos duplicados

--- bases-node/app.js
+++
@@ -1,7 +0,0 @@
-const { crearArchivo } = require('./helpers/multiplcacion');
-const argv = require('./config/yargs');
-
-crearArchivo(argv.b, argv.l, argv.h)
-    .then(nombreDeArchivo => console.log(nombreDeArchivo, 'creado'))
-    .catch((err) => console.log(err));
-
